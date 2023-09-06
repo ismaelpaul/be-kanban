bf57bc82e3683984268dba6305d88680380255b9
ismaelpaul <ismaafp@gmail.com>
2023-09-06 20:49:25 +0100

    Add selectBoardById file

--- models/boards.models.js
+++ models/boards.models.js
@@ -1,7 +1,11 @@
 const db = require('../db/connection');
 
 exports.selectBoards = () => {
-	return db.query(`SELECT * FROM boards`).then((result) => {
-		return result.rows;
-	});
+	return db.query(`SELECT * FROM boards`).then((result) => result.rows);
+};
+
+exports.selectBoardsById = (board_id) => {
+	return db
+		.query(`SELECT * FROM boards WHERE boards.board_id=$1;`, [board_id])
+		.then((result) => result.rows[0]);
 };
