96fa6dc477a7b4670cbf55029235dac929a183c8
ismaelpaul <ismaafp@gmail.com>
2023-09-06 20:48:29 +0100

    Add getBoardById function

--- controllers/boards.controllers.js
+++ controllers/boards.controllers.js
@@ -1,9 +1,18 @@
-const { selectBoards } = require('../models/boards.models');
+const { selectBoards, selectBoardsById } = require('../models/boards.models');
 
 exports.getBoards = (req, res, next) => {
 	selectBoards()
 		.then((boards) => {
 			res.status(200).send({ boards });
+		})
+		.catch(next);
+};
+
+exports.getBoardById = (req, res, next) => {
+	const { board_id } = req.params;
+	selectBoardsById(board_id)
+		.then((board) => {
+			res.status(200).send({ board });
 		})
 		.catch(next);
 };
