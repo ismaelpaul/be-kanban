3fb1e74c0ee4d51ebdf1c5db10c18ef521fbe0f8
ismaelpaul <ismaafp@gmail.com>
2023-09-07 19:53:48 +0100

    Add columns and user router

--- routes/api.router.js
+++ routes/api.router.js
@@ -1,10 +1,18 @@
 const express = require('express');
 
 const boardsRouter = require('./boards.router');
+const columnsRouter = require('./columns.router');
+const usersRouter = require('./users.router');
 
 const apiRouter = express.Router();
 
 // /boards
 apiRouter.use('/boards', boardsRouter);
+
+// /columns
+apiRouter.use('/columns', columnsRouter);
+
+// /users
+apiRouter.use('/users', usersRouter);
 
 module.exports = apiRouter;
