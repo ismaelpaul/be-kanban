34a1b3a185ebf28609f83cf0904d54dd908644f9
ismaelpaul <ismaafp@gmail.com>
2023-09-07 19:54:32 +0100

    Add selectColumnsByBoardId model function

--- models/boards.models.js
+++ models/boards.models.js
@@ -9,3 +9,18 @@ exports.selectBoardsById = (board_id) => {
 		.query(`SELECT * FROM boards WHERE boards.board_id=$1;`, [board_id])
 		.then((result) => result.rows[0]);
 };
+
+exports.selectColumnsByBoardId = (board_id) => {
+	return db
+		.query(
+			`SELECT 
+			columns.column_id, 
+			columns.board_id,
+			columns.name
+			 FROM columns LEFT JOIN boards ON columns.board_id = boards.board_id WHERE boards.board_id=$1;`,
+			[board_id]
+		)
+		.then((result) => {
+			return result.rows;
+		});
+};
