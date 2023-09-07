3a06df6d3b6238ec6cf98b9f1ec6b2636c53b952
ismaelpaul <ismaafp@gmail.com>
2023-09-07 19:56:23 +0100

    Add tests for GET method for /boards/:board_id/columns, /columns and /users endpoints

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -46,3 +46,67 @@ describe('/api/boards/:board_id', () => {
 		});
 	});
 });
+
+describe('/api/boards/:board_id/columns', () => {
+	describe('GET', () => {
+		test('200: responds with an array of columns for the given board id', () => {
+			return request(app)
+				.get('/api/boards/2/columns')
+				.expect(200)
+				.then((response) => {
+					const allColumns = response.body.columns;
+					expect(Array.isArray(allColumns)).toBe(true);
+					expect(allColumns.length > 0).toBe(true);
+					allColumns.forEach((column) => {
+						expect(column).toHaveProperty('column_id', expect.any(Number));
+						expect(column).toHaveProperty('board_id', expect.any(Number));
+						expect(column).toHaveProperty('name', expect.any(String));
+					});
+				});
+		});
+	});
+});
+
+describe('/api/columns', () => {
+	describe('GET', () => {
+		test('200: responds with an array of columns objects', () => {
+			return request(app)
+				.get('/api/columns')
+				.expect(200)
+				.then((res) => {
+					const allColumns = res.body.columns;
+					expect(typeof res.body).toBe('object');
+					expect(Array.isArray(allColumns)).toBe(true);
+					expect(allColumns.length > 0).toBe(true);
+					allColumns.forEach((board) => {
+						expect(board).toHaveProperty('column_id', expect.any(Number));
+						expect(board).toHaveProperty('board_id', expect.any(Number));
+						expect(board).toHaveProperty('name', expect.any(String));
+					});
+				});
+		});
+	});
+});
+
+describe('/api/users', () => {
+	describe('GET', () => {
+		test('200: return an array of users objects', () => {
+			return request(app)
+				.get('/api/users')
+				.expect(200)
+				.then((response) => {
+					const allUsers = response.body.users;
+					expect(typeof response.body).toBe('object');
+					expect(Array.isArray(allUsers)).toBe(true);
+					expect(allUsers.length > 0).toBe(true);
+					allUsers.forEach((board) => {
+						expect(board).toHaveProperty('user_id', expect.any(Number));
+						expect(board).toHaveProperty('first_name', expect.any(String));
+						expect(board).toHaveProperty('last_name', expect.any(String));
+						expect(board).toHaveProperty('email', expect.any(String));
+						expect(board).toHaveProperty('password', expect.any(String));
+					});
+				});
+		});
+	});
+});
