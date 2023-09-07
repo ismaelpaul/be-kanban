80669db92a33b46111fc3c167f0b1f4db886b3e1
ismaelpaul <ismaafp@gmail.com>
2023-09-07 19:52:11 +0100

    Add route to get columns by board_id

--- routes/boards.router.js
+++ routes/boards.router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {
 	getBoards,
 	getBoardById,
+	getColumnsByBoardId,
 } = require('../controllers/boards.controllers');
 
 const boardsRouter = express.Router();
@@ -9,5 +10,7 @@ const boardsRouter = express.Router();
 boardsRouter.route('/').get(getBoards);
 
 boardsRouter.route('/:board_id').get(getBoardById);
+
+boardsRouter.route('/:board_id/columns').get(getColumnsByBoardId);
 
 module.exports = boardsRouter;
