fd347e6ae2a1e631cf561af300297c24c7944374
ismaelpaul <ismaafp@gmail.com>
2023-09-09 08:51:10 +0100

    Add error handling

--- app.js
+++ app.js
@@ -7,4 +7,19 @@ app.use(express.json());
 
 app.use('/api', apiRouter);
 
+//error handling
+app.use('/*', (req, res) => {
+	res.status(404).send({ msg: 'Page not found.' });
+});
+
+app.use((err, req, res, next) => {
+	if (err.code === '22P02') {
+		res.status(400).send({ msg: 'Invalid ID.' });
+	} else if (err.status && err.msg) {
+		res.status(err.status).send({ msg: err.msg });
+	} else {
+		res.status(500).send({ msg: 'Internal Server Error!' });
+	}
+});
+
 module.exports = app;
