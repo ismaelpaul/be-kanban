c0393b298a84f50183d00cadfded50700e4354dc
ismaelpaul <ismaafp@gmail.com>
2023-09-10 20:07:23 +0100

    Add cors

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const apiRouter = require('./routes/api.router');
+const cors = require('cors');
 
 const app = express();
+
+const whitelist = ['http://localhost:5173'];
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (whitelist.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
+	credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
