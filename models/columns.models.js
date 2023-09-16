b9d5bfa25faf866643afcfd496b51cadf5779b29
ismaelpaul <ismaafp@gmail.com>
2023-09-16 18:28:08 +0100

    Add selectTasksByColumnsId model function

--- models/columns.models.js
+++ models/columns.models.js
@@ -1,5 +1,18 @@
 const db = require('../db/connection');
 
 exports.selectColumns = () => {
-	return db.query(`SELECT * FROM columns`).then((result) => result.rows);
+	return db.query(`SELECT * FROM columns`).then((result) => {
+		result.rows;
+	});
+};
+
+exports.selectTasksByColumnsId = (column_id) => {
+	return db
+		.query(
+			`SELECT tasks.task_id, tasks.column_id, tasks.description, tasks.title, tasks.status FROM tasks LEFT JOIN columns ON tasks.column_id = columns.column_id WHERE columns.column_id=$1;`,
+			[column_id]
+		)
+		.then((result) => {
+			return result.rows;
+		});
 };
