5a8c29ffaafb8a0034229e00bbe5003b04894fb5
ismaelpaul <ismaafp@gmail.com>
2023-09-16 18:27:34 +0100

    Add route to get tasks by column id

--- routes/columns.router.js
+++ routes/columns.router.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { getColumns } = require('../controllers/columns.controllers');
+const {
+	getColumns,
+	getTasksByColumnId,
+} = require('../controllers/columns.controllers');
 
 const columnsRouter = express.Router();
 
 columnsRouter.route('/').get(getColumns);
+
+columnsRouter.route('/:column_id/tasks').get(getTasksByColumnId);
 
 module.exports = columnsRouter;
