8b2f6286c5fd71e0f9deac78acf3c7c77800b253
ismaelpaul <ismaafp@gmail.com>
2023-09-17 21:31:13 +0100

    Add electSubtasksByTaskId model function

--- models/tasks.models.js
+++ models/tasks.models.js
@@ -5,3 +5,15 @@ exports.selectTasks = () => {
 		return result.rows;
 	});
 };
+
+exports.selectSubtasksByTaskId = (task_id) => {
+	console.log(task_id, '<<< id');
+	return db
+		.query(
+			`SELECT subtasks.subtask_id, subtasks.task_id, subtasks.title, subtasks.is_completed FROM subtasks LEFT JOIN tasks ON subtasks.task_id = tasks.task_id WHERE tasks.task_id=$1;`,
+			[task_id]
+		)
+		.then((result) => {
+			return result.rows;
+		});
+};
