5e06343258b5126c4022f93159e4c5173c0a95b5
ismaelpaul <ismaafp@gmail.com>
2023-09-17 21:30:26 +0100

    Add getSubtasksByTaskId controller function

--- controllers/tasks.controllers.js
+++ controllers/tasks.controllers.js
@@ -1,7 +1,20 @@
-const { selectTasks } = require('../models/tasks.models');
+const {
+	selectTasks,
+	selectSubtasksByTaskId,
+} = require('../models/tasks.models');
 
 exports.getTasks = (req, res, next) => {
 	selectTasks()
 		.then((tasks) => res.status(200).send({ tasks }))
+		.catch(next);
+};
+
+exports.getSubtasksByTaskId = (req, res, next) => {
+	const { task_id } = req.params;
+
+	selectSubtasksByTaskId(task_id)
+		.then((subtasks) => {
+			res.status(200).send({ subtasks });
+		})
 		.catch(next);
 };
