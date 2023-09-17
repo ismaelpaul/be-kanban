1baedfb9e25853fa1d70e874987efa2a98c27af2
ismaelpaul <ismaafp@gmail.com>
2023-09-17 21:32:03 +0100

    Add route for tasks/:task_id/subtasks endpoint

--- routes/tasks.router.js
+++ routes/tasks.router.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { getTasks } = require('../controllers/tasks.controllers');
+const {
+	getTasks,
+	getSubtasksByTaskId,
+} = require('../controllers/tasks.controllers');
 
 const tasksRouter = express.Router();
 
 tasksRouter.route('/').get(getTasks);
+
+tasksRouter.route('/:task_id/subtasks').get(getSubtasksByTaskId);
 
 module.exports = tasksRouter;
