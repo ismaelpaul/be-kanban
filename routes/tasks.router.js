5314ea035f02fd684be4f9e6f6c559b040b0b2ee
ismaelpaul <ismaafp@gmail.com>
2023-09-24 19:45:20 +0100

    Add route for /tasks/:task_id delete method

--- routes/tasks.router.js
+++ routes/tasks.router.js
@@ -2,11 +2,14 @@ const express = require('express');
 const {
 	getTasks,
 	getSubtasksByTaskId,
+	deleteTaskById,
 } = require('../controllers/tasks.controllers');
 
 const tasksRouter = express.Router();
 
 tasksRouter.route('/').get(getTasks);
+
+tasksRouter.route('/:task_id').delete(deleteTaskById);
 
 tasksRouter.route('/:task_id/subtasks').get(getSubtasksByTaskId);
 
