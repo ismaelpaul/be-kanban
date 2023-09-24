c3ea31d4951f0c92d72da91169dc0375b587a03f
ismaelpaul <ismaafp@gmail.com>
2023-09-24 12:59:16 +0100

    Add updateSubtaskCompletionById model function

--- models/subtasks.models.js
+++ models/subtasks.models.js
@@ -3,3 +3,17 @@ const db = require('../db/connection');
 exports.selectSubtasks = () => {
 	return db.query(`SELECT * FROM subtasks`).then((result) => result.rows);
 };
+
+exports.updateSubtaskCompletionById = (is_completed, subtask_id) => {
+	return db
+		.query(
+			`UPDATE subtasks 
+			SET is_completed = $1  
+			WHERE subtask_id = $2 
+			RETURNING *;`,
+			[is_completed, subtask_id]
+		)
+		.then((result) => {
+			result.rows[0];
+		});
+};
