41f3d6e19847904978b85819dee46e4f6e5ad0ca
ismaelpaul <ismaafp@gmail.com>
2023-09-24 12:58:22 +0100

    Clean up

--- models/tasks.models.js
+++ models/tasks.models.js
@@ -7,7 +7,6 @@ exports.selectTasks = () => {
 };
 
 exports.selectSubtasksByTaskId = (task_id) => {
-	console.log(task_id, '<<< id');
 	return db
 		.query(
 			`SELECT subtasks.subtask_id, subtasks.task_id, subtasks.title, subtasks.is_completed FROM subtasks LEFT JOIN tasks ON subtasks.task_id = tasks.task_id WHERE tasks.task_id=$1;`,
