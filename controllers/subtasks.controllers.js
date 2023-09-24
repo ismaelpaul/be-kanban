2a40ec8e7aada1e03a805f1ffee43ffa595fdeaa
ismaelpaul <ismaafp@gmail.com>
2023-09-24 12:59:44 +0100

    Add patchSubtaskCompletionById controller function

--- controllers/subtasks.controllers.js
+++ controllers/subtasks.controllers.js
@@ -1,9 +1,23 @@
-const { selectSubtasks } = require('../models/subtasks.models');
+const {
+	selectSubtasks,
+	updateSubtaskCompletionById,
+} = require('../models/subtasks.models');
 
 exports.getSubtasks = (req, res, next) => {
 	selectSubtasks()
 		.then((subtasks) => {
 			res.status(200).send({ subtasks });
+		})
+		.catch(next);
+};
+
+exports.patchSubtaskCompletionById = (req, res, next) => {
+	const { is_completed } = req.body;
+	const { subtask_id } = req.params;
+
+	updateSubtaskCompletionById(is_completed, subtask_id)
+		.then((subtask) => {
+			res.status(200).send({ subtask });
 		})
 		.catch(next);
 };
