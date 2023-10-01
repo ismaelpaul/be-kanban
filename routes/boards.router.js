b205d5753ad0dcd88373f0befe52f30da6deffef
ismaelpaul <ismaafp@gmail.com>
2023-10-01 17:29:29 +0100

    Add route for /boards/:board_id delete method

--- routes/boards.router.js
+++ routes/boards.router.js
@@ -3,13 +3,14 @@ const {
 	getBoards,
 	getBoardById,
 	getColumnsByBoardId,
+	deleteBoardById,
 } = require('../controllers/boards.controllers');
 
 const boardsRouter = express.Router();
 
 boardsRouter.route('/').get(getBoards);
 
-boardsRouter.route('/:board_id').get(getBoardById);
+boardsRouter.route('/:board_id').get(getBoardById).delete(deleteBoardById);
 
 boardsRouter.route('/:board_id/columns').get(getColumnsByBoardId);
 
