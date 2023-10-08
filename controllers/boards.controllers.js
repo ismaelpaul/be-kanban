9f2614a1c5e961547bcd8045d56524e9db886e18
ismaelpaul <ismaafp@gmail.com>
2023-10-08 20:40:43 +0100

    Add AddNewBoardAndColumns controller function

--- controllers/boards.controllers.js
+++ controllers/boards.controllers.js
@@ -3,7 +3,9 @@ const {
 	selectBoardsById,
 	selectColumnsByBoardId,
 	removeBoardById,
+	insertBoard,
 } = require('../models/boards.models');
+const { insertColumn } = require('../models/columns.models');
 
 exports.getBoards = (req, res, next) => {
 	selectBoards()
@@ -40,4 +42,37 @@ exports.getColumnsByBoardId = (req, res, next) => {
 			res.status(200).send({ columns });
 		})
 		.catch(next);
+};
+
+exports.addNewBoardAndColumns = (req, res, next) => {
+	const newBoard = req.body;
+	const { user_id, board_name } = newBoard;
+	const boardNameCapitalised = board_name
+		.trim()
+		.split(' ')
+		.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+		.join(' ');
+	insertBoard(user_id, boardNameCapitalised).then((board) => {
+		const board_id = board.board_id;
+		const columns = newBoard.columns;
+
+		const nonEmptyColumns = columns.filter(
+			(column) => column.column_name.trim() !== ''
+		);
+
+		if (nonEmptyColumns.length > 0) {
+			nonEmptyColumns.map((column) => {
+				const columnNameCapitalised = column.column_name
+					.trim()
+					.split(' ')
+					.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+					.join(' ');
+				insertColumn({ board_id, column_name: columnNameCapitalised })
+					.then(() => res.status(201).send({ board }))
+					.catch(next);
+			});
+		} else {
+			res.status(201).send({ board });
+		}
+	});
 };
