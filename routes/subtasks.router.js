fac0ffb73fa03bfe035bbcaf2a8b0c44a8c0aa88
ismaelpaul <ismaafp@gmail.com>
2023-10-22 21:19:35 +0100

    Add getSubtasksById controller function to /:subtask_id route get method

--- routes/subtasks.router.js
+++ routes/subtasks.router.js
@@ -2,12 +2,16 @@ const express = require('express');
 const {
 	getSubtasks,
 	patchSubtaskCompletionById,
+	getSubtasksById,
 } = require('../controllers/subtasks.controllers');
 
 const subtasksRouter = express.Router();
 
 subtasksRouter.route('/').get(getSubtasks);
 
-subtasksRouter.route('/:subtask_id').patch(patchSubtaskCompletionById);
+subtasksRouter
+	.route('/:subtask_id')
+	.get(getSubtasksById)
+	.patch(patchSubtaskCompletionById);
 
 module.exports = subtasksRouter;
