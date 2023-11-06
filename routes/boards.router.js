9c8202caebf28d564a900c8b376c0501b7f63f35
ismaelpaul <ismaafp@gmail.com>
2023-11-06 20:07:22 +0000

    Add route for /boards/:board_id endpoint patch method

--- routes/boards.router.js
+++ routes/boards.router.js
@@ -5,13 +5,18 @@ const {
 	getColumnsByBoardId,
 	deleteBoardById,
 	addNewBoardAndColumns,
+	patchBoardsAndColumns,
 } = require('../controllers/boards.controllers');
 
 const boardsRouter = express.Router();
 
 boardsRouter.route('/').get(getBoards).post(addNewBoardAndColumns);
 
-boardsRouter.route('/:board_id').get(getBoardById).delete(deleteBoardById);
+boardsRouter
+	.route('/:board_id')
+	.get(getBoardById)
+	.patch(patchBoardsAndColumns)
+	.delete(deleteBoardById);
 
 boardsRouter.route('/:board_id/columns').get(getColumnsByBoardId);
 
