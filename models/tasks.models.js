2edade63e8a0751ae8559ce2b51d6c0195c71b3f
ismaelpaul <ismaafp@gmail.com>
2023-11-08 19:49:10 +0000

    Add updateTaskPositionByTaskId model function

--- models/tasks.models.js
+++ models/tasks.models.js
@@ -40,3 +40,56 @@ exports.insertTask = (column_id, title, description, status) => {
 			return result.rows[0];
 		});
 };
+
+exports.updateTaskPositionByTaskId = (
+	newTaskPosition,
+	newColumn_id,
+	column_id,
+	task_id
+) => {
+	if (newColumn_id !== column_id) {
+		return db
+			.query('BEGIN')
+			.then(() => {
+				return db.query(
+					'UPDATE tasks SET column_id = $1 WHERE task_id = $2 RETURNING *;',
+					[newColumn_id, task_id]
+				);
+			})
+			.then(() => {
+				// Increment position for tasks in the new column
+				return db.query(
+					'UPDATE tasks SET position = position + 1 WHERE column_id = $1 AND position >= $2 AND task_id != $3 RETURNING *;',
+					[newColumn_id, newTaskPosition, task_id]
+				);
+			})
+			.then(() => {
+				// Decrement position for tasks in the original column
+				return db.query(
+					'UPDATE tasks SET position = position - 1 WHERE column_id = $1 AND position > $2 RETURNING *;',
+					[column_id, newTaskPosition]
+				);
+			})
+			.then(() => {
+				return db.query(
+					'UPDATE tasks SET position = $1 WHERE task_id = $2 RETURNING *;',
+					[newTaskPosition, task_id]
+				);
+			})
+			.then(() => {
+				return db.query('COMMIT');
+			})
+			.then((result) => {
+				return result.rows;
+			});
+	} else {
+		return db
+			.query('UPDATE tasks SET position = $1 WHERE task_id = $2 RETURNING *;', [
+				newTaskPosition,
+				task_id,
+			])
+			.then((result) => {
+				return result.rows;
+			});
+	}
+};
