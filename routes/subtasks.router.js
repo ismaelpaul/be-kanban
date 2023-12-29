d0daa2764098475596fcaf21d3fe90aead130cf0
ismaelpaul <ismaafp@gmail.com>
2023-12-29 19:40:00 +0000

    Add route for /subtasks endpoint delete method

--- routes/subtasks.router.js
+++ routes/subtasks.router.js
@@ -3,11 +3,12 @@ const {
 	getSubtasks,
 	patchSubtaskCompletionById,
 	getSubtasksById,
+	deleteSubtasks,
 } = require('../controllers/subtasks.controllers');
 
 const subtasksRouter = express.Router();
 
-subtasksRouter.route('/').get(getSubtasks);
+subtasksRouter.route('/').get(getSubtasks).delete(deleteSubtasks);
 
 subtasksRouter
 	.route('/:subtask_id')
