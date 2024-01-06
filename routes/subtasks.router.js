a9255a099708fa8df17cd5272f7ea3070d99784d
ismaelpaul <ismaafp@gmail.com>
2024-01-06 20:35:39 +0000

    Add route for /:subtask_id patch method and create a new route for /:subtask_id/completion endpoint patch method

--- routes/subtasks.router.js
+++ routes/subtasks.router.js
@@ -4,6 +4,7 @@ const {
 	patchSubtaskCompletionById,
 	getSubtasksById,
 	deleteSubtasks,
+	patchSubtaskTitleById,
 } = require('../controllers/subtasks.controllers');
 
 const subtasksRouter = express.Router();
@@ -13,6 +14,10 @@ subtasksRouter.route('/').get(getSubtasks).delete(deleteSubtasks);
 subtasksRouter
 	.route('/:subtask_id')
 	.get(getSubtasksById)
+	.patch(patchSubtaskTitleById);
+
+subtasksRouter
+	.route('/:subtask_id/completion')
 	.patch(patchSubtaskCompletionById);
 
 module.exports = subtasksRouter;
