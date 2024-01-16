24804a82222811fc71b66dd488dbf493abd107c2
ismaelpaul <ismaafp@gmail.com>
2024-01-16 20:47:19 +0000

    Add route for /columns endpoint delete method

--- routes/columns.router.js
+++ routes/columns.router.js
@@ -2,11 +2,12 @@ const express = require('express');
 const {
 	getColumns,
 	getTasksByColumnId,
+	deleteColumnsByColumnId,
 } = require('../controllers/columns.controllers');
 
 const columnsRouter = express.Router();
 
-columnsRouter.route('/').get(getColumns);
+columnsRouter.route('/').get(getColumns).delete(deleteColumnsByColumnId);
 
 columnsRouter.route('/:column_id/tasks').get(getTasksByColumnId);
 
