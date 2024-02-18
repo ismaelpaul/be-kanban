67f5b0e609299c869ed74705cffe2f7bcdaef1ca
ismaelpaul <ismaafp@gmail.com>
2024-02-18 20:00:47 +0000

    Add auth router

---
+++ routes/auth.router.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const passport = require('passport');
+
+const authRouter = express.Router();
+
+const CLIENT_URL = process.env.CLIENT_URL;
+
+authRouter.get('/login/success', (req, res) => {
+	console.log('successssssss');
+	console.log(req.user, '<<<<< user');
+	if (req.user) {
+		console.log(req.cookies, '<<<< cookies');
+		res.status(200).json({
+			success: true,
+			message: 'successfull',
+			user: req.user,
+			cookies: req.cookies,
+		});
+	}
+});
+
+authRouter.get('/login/failed', (req, res) => {
+	res.status(401).json({
+		success: false,
+		message: 'failure',
+	});
+});
+
+authRouter.get('/logout', (req, res) => {
+	req.logout();
+	res.redirect(CLIENT_URL);
+});
+
+authRouter
+	.route('/google')
+	.get(passport.authenticate('google', { scope: ['profile'] }));
+
+authRouter.route('/google/callback').get(
+	passport.authenticate('google', {
+		successRedirect: CLIENT_URL,
+		failureRedirect: '/login/failed',
+	})
+);
+
+module.exports = authRouter;
