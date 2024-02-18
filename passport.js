029b753828391a6b3264d86999fb1fec0129306d
ismaelpaul <ismaafp@gmail.com>
2024-02-18 19:58:56 +0000

    Add Google Strategy

---
+++ passport.js
@@ -0,0 +1,29 @@
+const passport = require('passport');
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: GOOGLE_CLIENT_ID,
+			clientSecret: GOOGLE_CLIENT_SECRET,
+			callbackURL: '/auth/google/callback',
+		},
+		function (accessToken, refreshToken, profile, cb, done) {
+			// TODO: Save user information
+
+			done(null, profile);
+		}
+	)
+);
+
+passport.serializeUser((user, done) => {
+	done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+	done(null, user);
+});
