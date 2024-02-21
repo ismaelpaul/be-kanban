16f2770e67d4a450406171901818d654a0d86d14
ismaelpaul <ismaafp@gmail.com>
2024-02-21 19:54:04 +0000

    Add github strategy

--- passport.js
+++ passport.js
@@ -1,21 +1,60 @@
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const GitHubStrategy = require('passport-github2').Strategy;
 
 passport.use(
 	new GoogleStrategy(
 		{
-			clientID: GOOGLE_CLIENT_ID,
-			clientSecret: GOOGLE_CLIENT_SECRET,
+			clientID: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 			callbackURL: '/auth/google/callback',
 		},
 		function (accessToken, refreshToken, profile, cb, done) {
 			// TODO: Save user information
 
+			const decodedToken = jwt.decode(profile.id_token, { complete: true });
+
 			done(null, profile);
+		}
+	)
+);
+
+passport.use(
+	new GitHubStrategy(
+		{
+			clientID: process.env.GITHUB_CLIENT_ID,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET,
+			callbackURL: '/auth/github/callback',
+		},
+		async function (accessToken, refreshToken, profile, done) {
+			try {
+				const emailResponse = await fetch(
+					'https://api.github.com/user/emails',
+					{
+						headers: {
+							Authorization: `Bearer ${accessToken}`,
+							'User-Agent': 'kanban',
+						},
+					}
+				);
+				const emailData = await emailResponse.json();
+
+				// Find the primary email
+				const primaryEmail = emailData.find((email) => email.primary);
+
+				if (primaryEmail) {
+					// TODO user registration
+				} else {
+					console.error('No primary email found.');
+					done(null, false);
+				}
+				done(null, profile);
+			} catch (error) {
+				console.error(error.message);
+				done(null, false);
+			}
 		}
 	)
 );
