dbff820a09dd53a7a4e47a7bed418a12f38e220d
ismaelpaul <ismaafp@gmail.com>
2024-05-21 19:12:42 +0100

    Add /login and /check-email endpoints for user route

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const { getUsers, registerUser } = require('../controllers/user.controllers');
+const {
+	getUsers,
+	registerUser,
+	checkEmail,
+	loginUser,
+} = require('../controllers/user.controllers');
 
 const usersRouter = express.Router();
 
 usersRouter.route('/').get(getUsers);
+usersRouter.route('/login').post(loginUser);
 usersRouter.route('/register').post(registerUser);
+usersRouter.route('/check-email').get(checkEmail);
 
 module.exports = usersRouter;
