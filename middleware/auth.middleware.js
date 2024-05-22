8c899d4add8d4ac5ef0a2ab95c3f96ed435143fd
ismaelpaul <ismaafp@gmail.com>
2024-05-22 19:56:44 +0100

    Clean up

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -6,10 +6,10 @@ exports.isAuthenticated = async (req, res, next) => {
 		const token = req.cookies.token;
 
 		if (!token) {
-			console.log('error token');
 			res.status(401);
 			throw new Error('Not authorized, please log in');
 		}
+
 		const verified = jwt.verify(token, process.env.JWT_SECRET);
 
 		const existingUser = await checkUserExistsById(verified);
