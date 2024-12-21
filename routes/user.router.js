e77a8c3ea2666f9ca4268b52967beeffffcbd356
ismaelpaul <ismaafp@gmail.com>
2024-12-21 19:59:42 +0000

    Add route to/team_members/:team_id endpoint get method

--- routes/user.router.js
+++ routes/user.router.js
@@ -8,11 +8,13 @@ const {
 	logoutUser,
 } = require('../controllers/user.controllers');
 const { isAuthenticated } = require('../middleware/auth.middleware');
+const { getUsersByTeamId } = require('../controllers/user.controllers');
 
 const usersRouter = express.Router();
 
 usersRouter.route('/').get(isAuthenticated, getUser);
 usersRouter.route('/:user_id').get(getUserById);
+usersRouter.route('/team_members/:team_id').get(getUsersByTeamId);
 usersRouter.route('/login').post(loginUser);
 usersRouter.route('/logout').post(logoutUser);
 usersRouter.route('/register').post(registerUser);
