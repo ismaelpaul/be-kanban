07878810f98b0f01952c97b23e3349e8fed1db72
ismaelpaul <ismaafp@gmail.com>
2024-12-22 17:04:28 +0000

    Add /teams/:team_id/boards endpoint get method

--- routes/teams.router.js
+++ routes/teams.router.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { getTeamByUserId } = require('../controllers/teams.controller');
+const {
+	getTeamByUserId,
+	getBoardsByTeamId,
+} = require('../controllers/teams.controller');
+const { isAuthenticated } = require('../middleware/auth.middleware');
 
 const teamsRouter = express.Router();
 
-teamsRouter.route('/').get(getTeamByUserId);
+teamsRouter.route('/').get(isAuthenticated, getTeamByUserId);
+teamsRouter.route('/:team_id/boards').get(getBoardsByTeamId);
 
 module.exports = teamsRouter;
