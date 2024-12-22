4064b2626b65e19d031d0ba77f9bedadeb142924
ismaelpaul <ismaafp@gmail.com>
2024-12-22 17:03:40 +0000

    Add getBoardsByTeamId controller function

--- controllers/teams.controller.js
+++ controllers/teams.controller.js
@@ -1,9 +1,20 @@
-const { selectTeamByUserId } = require('../models/teams.models');
+const {
+	selectTeamByUserId,
+	selectBoardsByTeamId,
+} = require('../models/teams.models');
 
 exports.getTeamByUserId = async (req, res) => {
 	const user_id = req.user.user_id;
 
 	selectTeamByUserId(user_id).then((teams) => {
 		res.status(200).send({ teams });
+	});
+};
+
+exports.getBoardsByTeamId = async (req, res) => {
+	const { team_id } = req.params;
+
+	selectBoardsByTeamId(team_id).then((boards) => {
+		res.status(200).send({ boards });
 	});
 };
