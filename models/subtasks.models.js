e822a1f91c96b3696f65a34e2104b8643ec26079
ismaelpaul <ismaafp@gmail.com>
2024-12-27 17:49:35 +0000

    Modify query string to include column_id

--- models/subtasks.models.js
+++ models/subtasks.models.js
@@ -15,10 +15,16 @@ exports.selectSubtasksById = (subtask_id) => {
 exports.updateSubtaskCompletionById = (is_completed, subtask_id) => {
 	return db
 		.query(
-			`UPDATE subtasks 
-			SET is_completed = $1  
-			WHERE subtask_id = $2 
-			RETURNING *;`,
+			`WITH updated_subtask AS (
+		   UPDATE subtasks
+		   SET is_completed = $1
+		   WHERE subtask_id = $2
+		   RETURNING task_id
+		 )
+		 SELECT subtasks.*, tasks.column_id
+		 FROM updated_subtask
+		 JOIN tasks ON tasks.task_id = updated_subtask.task_id
+		 JOIN subtasks ON subtasks.subtask_id = $2;`,
 			[is_completed, subtask_id]
 		)
 		.then((result) => {
