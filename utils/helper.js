03b2068be9ab6ad8a00f1d0cb5f0ef4576bda0ce
ismaelpaul <ismaafp@gmail.com>
2025-01-12 18:56:30 +0000

    Add fetchAvatarUrl helper function
    This function will fetch and avatar image based on the initals of the first_name and last_name of the user

--- utils/helper.js
+++ utils/helper.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const axios = require('axios');
 
 exports.hashPassword = (password) => {
 	const salt = bcrypt.genSaltSync();
@@ -13,4 +14,16 @@ exports.comparePassword = (raw, hash) => {
 exports.generateToken = (user_id) => {
 	const payload = { user_id };
 	return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '24h' });
+};
+
+exports.fetchAvatarUrl = async (first_name, last_name) => {
+	try {
+		const avatarApiUrl = `https://avatar.iran.liara.run/username?username=${first_name}+${last_name}`;
+		const response = await axios.get(avatarApiUrl);
+		return response.request.res.responseUrl;
+	} catch (error) {
+		console.error('Error fetching avatar URL:', error);
+
+		return 'https://i.ibb.co/4pDNDk1/avatar.png';
+	}
 };
