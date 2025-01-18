cb02a44fcaea4a023d6e19a01b641ccad7ca804f
ismaelpaul <ismaafp@gmail.com>
2025-01-18 17:28:24 +0000

    Add route for /team endpoint post method

--- routes/teams.router.js
+++ routes/teams.router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {
 	getTeamByUserId,
 	getBoardsByTeamId,
+	addTeam,
 } = require('../controllers/teams.controller');
 const { isAuthenticated } = require('../middleware/auth.middleware');
 
@@ -9,5 +10,6 @@ const teamsRouter = express.Router();
 
 teamsRouter.route('/').get(isAuthenticated, getTeamByUserId);
 teamsRouter.route('/:team_id/boards').get(getBoardsByTeamId);
+teamsRouter.route('/').post(isAuthenticated, addTeam);
 
 module.exports = teamsRouter;
