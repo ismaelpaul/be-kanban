19cddaf898cf895a883a33afd1afc42d9733d810
ismaelpaul <ismaafp@gmail.com>
2025-01-18 17:25:56 +0000

    Add addTeam controller function
    The function will also insert a default board and insert user into the team

--- controllers/teams.controller.js
+++ controllers/teams.controller.js
@@ -1,9 +1,12 @@
+const { insertBoard } = require('../models/boards.models');
 const {
 	selectTeamByUserId,
 	selectBoardsByTeamId,
+	insertTeam,
+	insertTeamMembersIntoTeam,
 } = require('../models/teams.models');
 
-exports.getTeamByUserId = async (req, res) => {
+exports.getTeamByUserId = (req, res) => {
 	const user_id = req.user.user_id;
 
 	selectTeamByUserId(user_id).then((teams) => {
@@ -11,10 +14,47 @@ exports.getTeamByUserId = async (req, res) => {
 	});
 };
 
-exports.getBoardsByTeamId = async (req, res) => {
+exports.getBoardsByTeamId = (req, res) => {
 	const { team_id } = req.params;
 
 	selectBoardsByTeamId(team_id).then((boards) => {
 		res.status(200).send({ boards });
 	});
+};
+
+exports.addTeam = (req, res) => {
+	const { name } = req.body;
+
+	let createdTeam;
+	let createdBoard;
+
+	insertTeam(name)
+		.then((team) => {
+			createdTeam = team;
+			const team_id = team.team_id;
+
+			return insertBoard(team_id, 'New Board');
+		})
+		.then((board) => {
+			createdBoard = board;
+
+			const user_id = req.user.user_id;
+			const team_id = createdTeam.team_id;
+			return insertTeamMembersIntoTeam(user_id, team_id, 'admin');
+		})
+		.then(() => {
+			res.status(201).json({
+				message: 'Team created successfully',
+				team: {
+					...createdTeam,
+					board: createdBoard,
+				},
+			});
+		})
+		.catch((err) => {
+			console.error(err);
+			res
+				.status(500)
+				.json({ message: 'Error creating team', error: err.message });
+		});
 };
