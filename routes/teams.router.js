be87a35bd24b0f512d680f155e59e4bf1d069553
ismaelpaul <ismaafp@gmail.com>
2025-01-21 19:51:35 +0000

    Add route for /:team_id endpoint patch method

--- routes/teams.router.js
+++ routes/teams.router.js
@@ -3,6 +3,7 @@ const {
 	getTeamByUserId,
 	getBoardsByTeamId,
 	addTeam,
+	patchTeamNameById,
 } = require('../controllers/teams.controller');
 const { isAuthenticated } = require('../middleware/auth.middleware');
 
@@ -11,5 +12,6 @@ const teamsRouter = express.Router();
 teamsRouter.route('/').get(isAuthenticated, getTeamByUserId);
 teamsRouter.route('/:team_id/boards').get(getBoardsByTeamId);
 teamsRouter.route('/').post(isAuthenticated, addTeam);
+teamsRouter.route('/:team_id').patch(patchTeamNameById);
 
 module.exports = teamsRouter;
