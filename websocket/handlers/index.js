5161b7317b355cdaff1db7794b7e99117197f1be
ismaelpaul <ismaafp@gmail.com>
2025-03-16 17:45:13 +0000

    Add columnsHandlers

--- websocket/handlers/index.js
+++ websocket/handlers/index.js
@@ -2,10 +2,12 @@ const tasksHandlers = require('./tasks');
 const subtasksHandlers = require('./subtasks');
 const boardsHandlers = require('./boards');
 const teamsHandlers = require('./teams');
+const columnsHandlers = require('./columns');
 
 module.exports = {
 	...tasksHandlers,
 	...subtasksHandlers,
 	...boardsHandlers,
 	...teamsHandlers,
+	...columnsHandlers,
 };
