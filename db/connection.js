882c9cd7fe0b863d85553af1062aae0d7a503c23
ismaelpaul <ismaafp@gmail.com>
2025-04-26 11:10:23 +0100

    Add condition to load env only in development

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,17 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
-require('dotenv').config({
-	path: `${__dirname}/../.env.${ENV}`,
-});
+if (ENV !== 'production') {
+	require('dotenv').config({
+		path: `${__dirname}/../.env.${ENV}`,
+	});
+}
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
 	throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
 const config =
-	ENV === 'production'
-		? {
-				connectionString: process.env.DATABASE_URL,
-		  }
-		: {};
+	ENV === 'production' ? { connectionString: process.env.DATABASE_URL } : {};
 
 module.exports = new Pool(config);
